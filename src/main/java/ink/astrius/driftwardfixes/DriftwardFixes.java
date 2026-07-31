65c5052e79329af0b6104a1e90fc90f554051742
Ivan Filipenkov <fox@vanutp.dev>
2026-07-31 04:51:57 +0200

    replace cobblestone with cobbled deepslate in the cobblestone generator

--- src/main/java/ink/astrius/driftwardfixes/DriftwardFixes.java
+++ src/main/java/ink/astrius/driftwardfixes/DriftwardFixes.java
@@ -1,12 +1,30 @@
 package ink.astrius.driftwardfixes;
 
+import net.minecraft.world.level.block.Blocks;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForgeMod;
+import net.neoforged.neoforge.fluids.FluidInteractionRegistry;
 
 @Mod("driftwardfixes")
 public class DriftwardFixes {
-    public DriftwardFixes(ModContainer container) {
+    public DriftwardFixes(IEventBus modEventBus, ModContainer container) {
         container.registerConfig(ModConfig.Type.SERVER, Config.SPEC);
+        modEventBus.addListener(DriftwardFixes::onSetup);
+    }
+
+    public static void onSetup(final FMLCommonSetupEvent event) {
+        FluidInteractionRegistry.addInteraction(
+            NeoForgeMod.LAVA_TYPE.value(),
+            new FluidInteractionRegistry.InteractionInformation(
+                NeoForgeMod.WATER_TYPE.value(),
+                (fluidState) -> fluidState.isSource()
+                    ? Blocks.OBSIDIAN.defaultBlockState()
+                    : Blocks.COBBLED_DEEPSLATE.defaultBlockState()
+            )
+        );
     }
 }
