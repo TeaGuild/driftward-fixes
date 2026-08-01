4d03c5432d530a81279a77673a773e6a1d07c0cb
Ivan Filipenkov <fox@vanutp.dev>
2026-08-01 03:37:19 +0200

    replace stone with deepslate when generating from lava+water

---
+++ src/main/java/ink/astrius/driftwardfixes/mixin/LavaFluidMixin.java
@@ -0,0 +1,24 @@
+package ink.astrius.driftwardfixes.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.material.LavaFluid;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(LavaFluid.class)
+public class LavaFluidMixin {
+    @ModifyExpressionValue(
+        method = "spreadTo",
+        at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/world/level/block/Blocks;STONE:Lnet/minecraft/world/level/block/Block;",
+            opcode = Opcodes.GETSTATIC
+        )
+    )
+    public Block generateDeepslate(Block original) {
+        return Blocks.DEEPSLATE;
+    }
+}
