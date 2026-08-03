2ae3946e1f235af23329bdc066f7619fe1b4cf0c
Ivan Filipenkov <fox@vanutp.dev>
2026-08-03 10:55:34 +0200

    damage hostile mobs by trains when `trainsCauseDamage` is disabled

---
+++ src/main/java/ink/astrius/driftwardfixes/mixin/ContraptionColliderMixin.java
@@ -0,0 +1,28 @@
+package ink.astrius.driftwardfixes.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.simibubi.create.content.contraptions.ContraptionCollider;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.MobCategory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ContraptionCollider.class)
+public class ContraptionColliderMixin {
+    @ModifyExpressionValue(
+        method = "handleDamageFromTrain",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/createmod/catnip/config/ConfigBase$ConfigBool;get()Ljava/lang/Object;"
+        )
+    )
+    private static Object damageHostileWhenDamageDisabled(
+        Object original,
+        @Local(argsOnly = true) Entity entity
+    ) {
+        final var orig = (boolean) original;
+        final var isMonster = entity.getClassification(false) == MobCategory.MONSTER;
+        return orig || isMonster;
+    }
+}
